2817b0afdde2e3dad1777503c99b44c4846d86a5
krtkeyan <karthikepsilon@gmail.com>
2016-12-31 17:28:11 +0530

    Redis-Cache-FasterLogin

--- src/config/routes/authRoutes.js
+++ src/config/routes/authRoutes.js
@@ -3,26 +3,46 @@ var bodyParser = require("body-parser");
 var mongodb = require("mongodb").MongoClient;
 var authRoutes = express.Router();
 var passport = require("passport");
-
+var mongoose = require("mongoose");
+var bcrypt =  require("bcrypt");
+var redis = require("redis");
+var redisClient = redis.createClient();
 
 authRoutes.route("/signup").post(function(req,res){
+     mongoose.connect("mongodb://karthik:password@ds145828.mlab.com:45828/inventory",function(err,db){
+        var hash = bcrypt.hashSync(req.body.password,10);        
+        var schema = mongoose.Schema({
+            username:{
+                type:String,
+                required:true
+            },
+            _id:{
+                type:String,
+                required:true,
+                match: /.+@.+\..+/,
+                lowercase: true
+            },
+            password:{
+                type:String,
+                required:true
+            }
+        });
+        var User = mongoose.model("Users",schema,"users");
 
-   
-    mongodb.connect("mongodb://karthik:password@ds145828.mlab.com:45828/inventory",function(err,db){
-        var collection = db.collection("users");
-        
         var userRecord = {
             username:req.body.name,
             _id:req.body.email,
-            password:req.body.password
+            password:hash
         };
+        var user = new User(userRecord);
 
-        collection.insert(userRecord,function(err,results){
+        user.save(function(err){
             if(err){ 
                 res.json({error:"record exists"});
             }
             else{
-                 res.json(results.ops[0]);
+                 redisClient.hmset("user","email",userRecord._id,"password",userRecord.password,redis.print);
+                 res.json("saved");
             }
         });    
     });
--- src/config/strategies/local.strategy.js
+++ src/config/strategies/local.strategy.js
@@ -1,25 +1,40 @@
 var LocalStrategy = require("passport-local").Strategy;
 var passport = require("passport");
 var mongodb = require("mongodb").MongoClient;
-
+var bcrypt = require("bcrypt");
+var redis = require("redis");
+var redisClient = redis.createClient();
 module.exports = function(){
     passport.use(new LocalStrategy({
         usernameField:"email",
         passwordField:"password"
     },function(username,password,done){
-      
-        mongodb.connect("mongodb://karthik:password@ds145828.mlab.com:45828/inventory",function(err,db){
-            var collection = db.collection("users");
-            collection.findOne({_id:username},function(err,results){
-                if(results.password === password){
-                    done(null,username);
-                }
-                else{
-                    done(null,false,{
-                        message:"Wrong Password"
-                    })
-                }
-            });
+        redisClient.hget("user","email",(err,res)=>{
+            console.log(res);
+            if(res==username){
+                redisClient.hget("user","password",(err,pass)=>{
+                    if(bcrypt.compareSync(password,pass)){
+                        console.log("Redis Magic")
+                        done(null,username);
+                    }          
+                })
+            }else{
+                mongodb.connect("mongodb://karthik:password@ds145828.mlab.com:45828/inventory",function(err,db){
+                var collection = db.collection("users");
+                collection.findOne({_id:username},function(err,results){
+                    if(bcrypt.compareSync(password,results.password)){
+                        console.log("Mongo");
+                        done(null,username);
+                    }
+                    else{
+                        done(null,false,{
+                            message:"Wrong Password"
+                        })
+                    }
+                });
         })
+            }
+        })
+       
     }));
 }
